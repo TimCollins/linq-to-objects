062ba4b271b2331313073c4781757389a5473946
Tim Collins <tim.collins@hpe.com>
2018-10-21 18:45:33 +0100

    Added initial version of Where.

---
+++ src/LinqToObjects/Where.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (TSource item in source)
+            {
+                yield return item;
+            }
+        }
+    }
+}
