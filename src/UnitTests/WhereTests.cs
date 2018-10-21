74f854ab32ada32f9e7a60d82bd23c939b992a7b
Tim Collins <tim.collins@hpe.com>
2018-10-21 17:22:00 +0100

    Added initial test.

---
+++ src/UnitTests/WhereTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class WhereTests
+    {
+        [Test]
+        public void SimpleFiltering()
+        {
+            int[] source = { 1, 3, 4, 2, 8, 1 };
+
+            var result = source.Where(x => x < 4);
+
+            Assert.AreEqual(result, new int[] { 1, 3, 2, 1 });
+        }
+    }
+}
