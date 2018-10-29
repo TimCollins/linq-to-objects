9911d4e693e5b1cff3e6c3aa02afd296349840ad
Tim Collins <tim.collins@hpe.com>
2018-10-29 17:56:14 +0000

    Added initial implementation of Select.

---
+++ src/LinqToObjects/Select.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        // Returns an enumerable collection of the TResult type (since the TSource type is getting projected to that type)
+        // Takes an enumerable collection of TSource as input (as an extension method)
+        // Takes a delegate which takes a TSource element and returns a TResult element
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+
+            return null;
+        }
+    }
+}
