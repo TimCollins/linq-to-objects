2b7ff3b441c8e82a15a198288c0a8b996bc1ebbf
Tim Collins <tim.collins@hpe.com>
2018-11-02 12:07:58 +0000

    Added initial crappy implementation and a failing test.

---
+++ src/LinqToObjects/Empty.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Empty<TResult>()
+        {
+            yield return default(TResult);
+        }
+    }
+}
---
+++ src/UnitTests/EmptyTests.cs
@@ -0,0 +1,17 @@
+﻿using LinqToObjects;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class EmptyTests
+    {
+        [Test]
+        public void ShouldReturnEmptySequence()
+        {
+            var output = Enumerable.Empty<int>();
+            var enumerator = output.GetEnumerator();
+            Assert.IsFalse(enumerator.MoveNext());
+        }
+    }
+}
