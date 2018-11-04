5dd1083ba7d67f68430173185d17b6d880f587f2
Tim Collins <tim.collins@hpe.com>
2018-11-04 18:05:04 +0000

    Added placeholder test and implementation classes.

---
+++ src/LinqToObjects/Repeat.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Repeat<TResult>(TResult element, int count)
+        {
+            return null;
+        }
+    }
+}
