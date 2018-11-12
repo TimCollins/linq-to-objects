425385c1b031da9a0f1a7134ee9cb723152c9ae4
Tim Collins <tim.collins@hpe.com>
2018-11-12 19:49:35 +0000

    Added initial implementation.

--- src/LinqToObjects/Count.cs
+++ src/LinqToObjects/Count.cs
@@ -1,6 +1,37 @@
-﻿namespace LinqToObjects
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
 {
     public static partial class Enumerable
     {
+        public static int Count<T>(this IEnumerable<T> input)
+        {
+            var count = 0;
+            var enumerator = input.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        public static int Count<T>(this IEnumerable<T> input, Func<int, bool> predicate)
+        {
+            var count = 0;
+            var enumerator = input.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                if (predicate(Convert.ToInt32(enumerator.Current)))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
     }
 }
