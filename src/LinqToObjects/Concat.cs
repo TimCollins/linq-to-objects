b4e61b97e52d017c6c0e9d35fa522a9f9d8a8f0e
Tim Collins <tim.collins@hpe.com>
2018-11-14 19:56:01 +0000

    Added tests and implementation.

--- src/LinqToObjects/Concat.cs
+++ src/LinqToObjects/Concat.cs
@@ -1,6 +1,43 @@
-﻿namespace LinqToObjects
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
 {
-    public class Concat
+    public static partial class Enumerable
     {
+        public static IEnumerable<TSource> Concat<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second)
+        {
+            if (first == null)
+            {
+                throw new ArgumentNullException("first");
+            }
+
+            if (second == null)
+            {
+                throw new ArgumentNullException("second");
+            }
+
+            // The deferred execution of the concatenation can't exist in the same scope as the 
+            // immediate execution of the argument validation.
+            return ConcatImpl(first, second);
+        }
+
+        private static IEnumerable<TSource> ConcatImpl<TSource>(IEnumerable<TSource> first, IEnumerable<TSource> second)
+        {
+            // The concatenation is basically just returning all of the first 
+            // set and then all of the second set.
+            foreach (TSource item in first)
+            {
+                yield return item;
+            }
+
+            // Avoid hanging on to a reference that isn't used any more.
+            first = null;
+
+            foreach (TSource item in second)
+            {
+                yield return item;
+            }
+        }
     }
 }
--- src/UnitTests/ConcatTests.cs
+++ src/UnitTests/ConcatTests.cs
@@ -1,9 +1,71 @@
-﻿using NUnit.Framework;
+﻿using LinqToObjects;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+//using System.Linq;
 
 namespace UnitTests
 {
     [TestFixture]
     public class ConcatTests
     {
+        [Test]
+        public void WillThrowExceptionForNullFirstArgument()
+        {
+            List<string> first = null;
+            var second = new List<string> { "something", "else" };
+            Assert.Throws<ArgumentNullException>(() => { first.Concat(second); });
+        }
+
+        [Test]
+        public void WillThrowExceptionForNullSecondArgument()
+        {
+            var first = new List<string>();
+            Assert.Throws<ArgumentNullException>(() => { first.Concat(null); });
+        }
+
+        [Test]
+        public void SimpleConcatenation()
+        {
+            var first = new string[] { "a", "b" };
+            var second = new string[] { "c", "d" };
+
+            var output = first.Concat(second);
+
+            output.AssertSequenceEqual("a", "b", "c", "d");
+        }
+
+        [Test]
+        public void FirstSequenceIsNotAccessedBeforeUse()
+        {
+            var first = new ThrowingEnumerable();
+            var second = new int[] { 5 };
+
+            var query = first.Concat(second);
+
+            using (var iterator = query.GetEnumerator())
+            {
+                Assert.Throws<InvalidOperationException>(() => iterator.MoveNext());
+            }
+        }
+
+        [Test]
+        public void SecondSequenceIsNotAccessedBeforeUse()
+        {
+            var first = new int[] { 5 };
+            var second = new ThrowingEnumerable();
+
+            var query = first.Concat(second);
+
+            using (var iterator = query.GetEnumerator())
+            {
+                // MoveNext will succeed as there is an element in first.
+                Assert.IsTrue(iterator.MoveNext());
+                Assert.AreEqual(5, iterator.Current);
+
+                // This will fail as the second sequence doesn't support MoveNext().
+                Assert.Throws<InvalidOperationException>(() => iterator.MoveNext());
+            }
+        }
     }
 }
