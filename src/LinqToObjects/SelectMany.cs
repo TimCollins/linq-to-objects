681fec0cc164317ad7487156510bc240c100e51b
Tim Collins <tim.collins@hpe.com>
2018-11-17 16:00:42 +0000

    Added more explicit validation. Reformatted method signatures for clarity.

--- src/LinqToObjects/SelectMany.cs
+++ src/LinqToObjects/SelectMany.cs
@@ -5,7 +5,9 @@ namespace LinqToObjects
 {
     public static partial class Enumerable
     {
-        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> selector)
         {
             if (source == null)
             {
@@ -20,13 +22,120 @@ namespace LinqToObjects
             return SelectManyImpl(source, selector);
         }
 
-        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(
+            IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> selector)
         {
             foreach (TSource item in source)
             {
                 foreach (TResult result in selector(item))
                 {
                     yield return result;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TResult>> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException("selector");
+            }
+
+            return SelectManyImpl(source, selector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(
+                    IEnumerable<TSource> source,
+                    Func<TSource, int, IEnumerable<TResult>> selector)
+        {
+            var index = 0;
+
+            foreach (TSource item in source)
+            {
+                foreach (TResult result in selector(item, index++))
+                {
+                    yield return result;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TCollection, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TCollection>> collectionSelector,
+            Func<TSource, TCollection, TResult> resultSelector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (collectionSelector == null)
+            {
+                throw new ArgumentNullException("collectionSelector");
+            }
+
+            if (resultSelector == null)
+            {
+                throw new ArgumentNullException("resultSelector");
+            }
+
+            return SelectManyImpl(source, collectionSelector, resultSelector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TCollection, TResult>(IEnumerable<TSource> source, Func<TSource, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector)
+        {
+            foreach (TSource item in source)
+            {
+                foreach (TCollection collectionItem in collectionSelector(item))
+                {
+                    yield return resultSelector(item, collectionItem);
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TCollection, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TCollection>> collectionSelector,
+            Func<TSource, TCollection, TResult> resultSelector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (collectionSelector == null)
+            {
+                throw new ArgumentNullException("collectionSelector");
+            }
+
+            if (resultSelector == null)
+            {
+                throw new ArgumentNullException("resultSelector");
+            }
+
+            return SelectManyImpl(source, collectionSelector, resultSelector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TCollection, TResult>(
+            IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TCollection>> collectionSelector,
+            Func<TSource, TCollection, TResult> resultSelector)
+        {
+            var index = 0;
+
+            foreach (TSource item in source)
+            {
+                foreach (TCollection collectionItem in collectionSelector(item, index++))
+                {
+                    yield return resultSelector(item, collectionItem);
                 }
             }
         }
