19e84c0cd207a3f422f5624d9f33e06b28665859
Tim Collins <tim.collins@hpe.com>
2018-11-17 16:46:53 +0000

    Added initial implementation.

---
+++ src/LinqToObjects/Any.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static bool Any<TSource>(this IEnumerable<TSource> source)
+        {
+            return false;
+        }
+
+        
+        public static bool Any<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            return false;
+        }
+    }
+}
