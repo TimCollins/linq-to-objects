6ebfd09da310663018d1535d0e0f9ca74893a7aa
Tim Collins <tim.collins@hpe.com>
2018-11-17 16:57:35 +0000

    Updated implementation.

--- src/LinqToObjects/Any.cs
+++ src/LinqToObjects/Any.cs
@@ -7,13 +7,58 @@ namespace LinqToObjects
     {
         public static bool Any<TSource>(this IEnumerable<TSource> source)
         {
-            return false;
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return AnyImpl(source);
         }
 
-        
+        private static bool AnyImpl<TSource>(IEnumerable<TSource> source)
+        {
+            var count = 0;
+            using (var iterator = source.GetEnumerator())
+            {
+                while (iterator.MoveNext())
+                {
+                    count++;
+                }
+            }
+
+            return count > 0;
+        }
+
         public static bool Any<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
         {
-            return false;
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException("predicate");
+            }
+
+            return AnyImpl(source, predicate);
+        }
+
+        private static bool AnyImpl<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            var count = 0;
+            using (var iterator = source.GetEnumerator())
+            {
+                while (iterator.MoveNext())
+                {
+                    if (predicate(iterator.Current))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count > 0;
         }
     }
 }
