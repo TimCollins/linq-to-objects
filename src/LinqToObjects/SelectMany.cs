257d8cbfa0eff1be468a6732b619235dbea35767
Tim Collins <tim.collins@hpe.com>
2018-11-17 15:04:53 +0000

    Added initial implementation of SelectMany.

---
+++ src/LinqToObjects/SelectMany.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException("selector");
+            }
+
+            return SelectManyImpl(source, selector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        {
+            foreach (TSource item in source)
+            {
+                foreach (TResult result in selector(item))
+                {
+                    yield return result;
+                }
+            }
+        }
+    }
+}
