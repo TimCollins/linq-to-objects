6edf6d1980cb1f1f00f551489535c4f7625d89a8
Tim Collins <tim.collins@hpe.com>
2018-11-18 20:04:56 +0000

    Completed implementation.

--- src/LinqToObjects/FirstOrDefault.cs
+++ src/LinqToObjects/FirstOrDefault.cs
@@ -12,7 +12,15 @@ namespace LinqToObjects
                 throw new ArgumentNullException("source");
             }
 
-            return default(TSource);
+            using (IEnumerator<TSource> iterator = source.GetEnumerator())
+            {
+                if (!iterator.MoveNext())
+                {
+                    return default(TSource);
+                }
+
+                return iterator.Current;
+            }
         }
 
         public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
@@ -26,6 +34,15 @@ namespace LinqToObjects
             {
                 throw new ArgumentNullException("predicate");
             }
+
+            foreach (TSource item in source)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+            }
+
             return default(TSource);
         }
     }
