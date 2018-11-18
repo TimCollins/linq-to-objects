0a7cbbea9b16757abdaa2ceafe7076052015ab22
Tim Collins <tim.collins@hpe.com>
2018-11-18 17:28:40 +0000

    Added initial implementation and some tests.

---
+++ src/UnitTests/FirstTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class FirstTests
+    {
+        [Test]
+        public void NullSourceWithoutPredicate()
+        {
+            int[] source = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.First());
+        }
+
+        [Test]
+        public void NullSourceWithPredicate()
+        {
+            int[] source = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.First(n => n > 2));
+        }
+
+        [Test]
+        public void NullPredicate()
+        {
+            int[] source = { 1, 3, 5 };
+
+            Assert.Throws<ArgumentNullException>(() => source.First(null));
+        }
+
+        [Test]
+        public void EmptySequenceWithoutPredicate()
+        {
+            int[] source = { };
+
+            Assert.Throws<InvalidOperationException>(() => source.First());
+        }
+    }
+}
