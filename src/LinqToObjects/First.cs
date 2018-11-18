f3455e480ab895ea7f153f9fc719ac3d0fbab5e3
Tim Collins <tim.collins@hpe.com>
2018-11-18 17:44:40 +0000

    Started adding implementation.

--- src/LinqToObjects/First.cs
+++ src/LinqToObjects/First.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LinqToObjects
 {
     public static partial class Enumerable
     {
+        public static TSource First<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
 
+            return default(TSource);
+        }
+
+        public static TSource First<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return default(TSource);
+        }
     }
 }
