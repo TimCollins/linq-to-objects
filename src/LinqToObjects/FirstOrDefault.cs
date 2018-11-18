30113bde853f85104c8445e84cba548c25815b9a
Tim Collins <tim.collins@hpe.com>
2018-11-18 19:58:45 +0000

    Added implementation based on First().

--- src/LinqToObjects/FirstOrDefault.cs
+++ src/LinqToObjects/FirstOrDefault.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LinqToObjects
 {
     public static partial class Enumerable
     {
+        public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return default(TSource);
+        }
+
+        public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException("predicate");
+            }
+            return default(TSource);
+        }
     }
 }
