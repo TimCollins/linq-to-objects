e7826a90ffe469b907178d24dc8f9c1af44c55b0
Tim Collins <tim.collins@hpe.com>
2018-11-18 20:42:15 +0000

    Added implementation of Last().

--- src/LinqToObjects/Last.cs
+++ src/LinqToObjects/Last.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LinqToObjects
 {
     public static partial class Enumerable
     {
+        public static TSource Last<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            var list = new List<TSource>(source);
+
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("source");
+            }
+
+
+            return list[list.Count - 1];
+        }
+
+        public static TSource Last<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException("predicate");
+            }
+
+            var list = new List<TSource>(source);
+
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("source");
+            }
+
+            for (var i = list.Count - 1; i >= 0; i--)
+            {
+                var item = list[i];
+                if (predicate(list[i]))
+                {
+                    return item;
+                }
+            }
+
+            throw new InvalidOperationException("source");
+        }
     }
 }
