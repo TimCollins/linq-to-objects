21d51307e2412d95a370fed91905b5c28662adc5
Tim Collins <tim.collins@hpe.com>
2018-11-18 20:17:40 +0000

    Added some tests derived from First().

---
+++ src/UnitTests/LastTests.cs
@@ -0,0 +1,106 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class LastTests
+    {
+        [Test]
+        public void NullSourceWithoutPredicate()
+        {
+            int[] source = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.Last());
+        }
+
+        [Test]
+        public void NullSourceWithPredicate()
+        {
+            int[] source = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.Last(n => n > 2));
+        }
+
+        [Test]
+        public void NullPredicate()
+        {
+            int[] source = { 1, 3, 5 };
+
+            Assert.Throws<ArgumentNullException>(() => source.Last(null));
+        }
+
+        [Test]
+        public void EmptySequenceWithoutPredicate()
+        {
+            int[] source = { };
+
+            Assert.Throws<InvalidOperationException>(() => source.Last());
+        }
+
+        [Test]
+        public void SingleElementSequenceWithoutPredicate()
+        {
+            int[] source = { 5 };
+
+            Assert.AreEqual(5, source.Last());
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithoutPredicate()
+        {
+            int[] source = { 5, 10 };
+
+            Assert.AreEqual(10, source.Last());
+        }
+
+        [Test]
+        public void EmptySequenceWithPredicate()
+        {
+            int[] source = { };
+
+            Assert.Throws<InvalidOperationException>(() => source.Last(n => n > 2));
+        }
+
+        [Test]
+        public void SingleElementSequenceWitMatchingPredicate()
+        {
+            int[] source = { 5 };
+
+            Assert.AreEqual(5, source.Last(n => n > 2));
+        }
+
+        [Test]
+        public void SingleElementSequenceWithNonMatchingPredicate()
+        {
+            int[] source = { 5 };
+
+            Assert.Throws<InvalidOperationException>(() => source.Last(n => n > 10));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithNoPredicateMatches()
+        {
+            int[] source = { 1, 2, 2, 1 };
+
+            Assert.Throws<InvalidOperationException>(() => source.Last(n => n > 10));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithSinglePredicateMatch()
+        {
+            int[] source = { 1, 2, 3, 4 };
+
+            Assert.AreEqual(4, source.Last(n => n > 3));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithMultiplePredicateMatches()
+        {
+            int[] source = { 1, 2, 3, 4, 8, 16 };
+
+            Assert.AreEqual(16, source.Last(n => n > 3));
+        }
+    }
+}
