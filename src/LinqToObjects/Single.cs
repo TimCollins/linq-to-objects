1bede776874bf20f0019e46b27a526f2544271d4
Tim Collins <tim.collins@hpe.com>
2018-11-22 21:36:25 +0000

    Added initial implementation of Single()

---
+++ src/LinqToObjects/Single.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static TSource Single<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            return default(TSource);
+        }
+
+        public static TSource Single<TSource>(this IEnumerable<TSource> source)
+        {
+            return default(TSource);
+        }
+    }
+}
