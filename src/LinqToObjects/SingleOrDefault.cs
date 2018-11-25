36572ebe9c0f6d5651b1570af58faf4e60ee0c56
Tim <tim@timcollins.ie>
2018-11-25 13:02:29 +0000

    Added initial implementation.

---
+++ src/LinqToObjects/SingleOrDefault.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static TSource SingleOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException("predicate");
+            }
+
+            var retVal = default(TSource);
+            var foundAny = false;
+
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    if (foundAny)
+                    {
+                        throw new InvalidOperationException("Sequence contained multiple matching elements.");
+                    }
+
+                    foundAny = true;
+                    retVal = item;
+                }
+            }
+
+            if (!foundAny)
+            {
+                throw new InvalidOperationException("No item found for predicate.");
+            }
+
+            return retVal;
+        }
+
+        public static TSource SingleOrDefault<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            using (IEnumerator<TSource> iterator = source.GetEnumerator())
+            {
+                if (!iterator.MoveNext())
+                {
+                    throw new InvalidOperationException("Sequence was empty.");
+                }
+
+                var retVal = iterator.Current;
+
+                if (iterator.MoveNext())
+                {
+                    throw new InvalidOperationException("Sequence contained multiple elements.");
+                }
+
+                return retVal;
+            }
+        }
+    }
+}
