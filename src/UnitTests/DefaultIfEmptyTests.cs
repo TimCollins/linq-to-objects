8604556a57d77b1994365b9693a174756179c189
Tim <tim@timcollins.ie>
2018-11-25 13:39:36 +0000

    Added unit tests.

---
+++ src/UnitTests/DefaultIfEmptyTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class DefaultIfEmptyTests
+    {
+        [Test]
+        public void NullSourceNoDefaultValue()
+        {
+            int[] source = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.DefaultIfEmpty());
+        }
+
+        [Test]
+        public void NullSourceWithDefaultValue()
+        {
+            int[] source = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.DefaultIfEmpty(5));
+        }
+
+        [Test]
+        public void EmptySequenceNoDefaultValue()
+        {
+            Enumerable.Empty<int>().DefaultIfEmpty(5).AssertSequenceEqual(5);
+        }
+
+        [Test]
+        public void EmptySequenceWithDefaultValue()
+        {
+            int[] source = { 3, 1, 4 };
+
+            source.DefaultIfEmpty().AssertSequenceEqual(source);
+        }
+
+        [Test]
+        public void NonEmptySequenceWithDefaultValue()
+        {
+            int[] source = { 3, 1, 4 };
+
+            source.DefaultIfEmpty(5).AssertSequenceEqual(source);
+        }
+    }
+}
