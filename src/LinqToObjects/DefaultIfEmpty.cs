becc50447303dc43b9f453faa0d86fdf3ff3b786
Tim <tim@timcollins.ie>
2018-11-25 14:20:18 +0000

    Added initial implementation.

---
+++ src/LinqToObjects/DefaultIfEmpty.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> DefaultIfEmpty<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return null;
+        }
+
+        public static IEnumerable<TSource> DefaultIfEmpty<TSource>(this IEnumerable<TSource> source, TSource defaultValue)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return DefaultIfEmptyImpl(source, defaultValue);
+        }
+
+        private static IEnumerable<TSource> DefaultIfEmptyImpl<TSource>(IEnumerable<TSource> source, TSource defaultValue)
+        {
+            yield return defaultValue;
+        }
+    }
+}
