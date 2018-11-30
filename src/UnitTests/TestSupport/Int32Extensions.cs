ca6b9d721f53908568faf4ec1b65cb2e7e7172b4
Tim <tim@timcollins.ie>
2018-11-30 21:03:22 +0000

    Added Int32Extensions.cs.

---
+++ src/UnitTests/TestSupport/Int32Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace UnitTests.TestSupport
+{
+    public static class Int32Extensions
+    {
+        /// <summary>
+        /// Convert an Int32 to a string using the invariant culture,
+        /// without explicitly having to use CultureInfo.InvariantCulture everywhere.
+        /// </summary>
+        public static string ToInvariantString(this int value)
+        {
+            return value.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
