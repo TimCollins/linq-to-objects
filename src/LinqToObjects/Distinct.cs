4872f1883340f1d06861cd1e0ab3fd9589ce9b4b
Tim <tim@timcollins.ie>
2018-12-19 20:39:16 +0000

    Added initial implementation

---
+++ src/LinqToObjects/Distinct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Distinct<TSource>(this IEnumerable<TSource> source)        
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return null;
+        }
+
+        // IEqualityComparer<TSource>
+        public static IEnumerable<TSource> Distinct<TSource>(this IEnumerable<TSource> source, StringComparer comparer)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return null;
+        }
+    }
+}
