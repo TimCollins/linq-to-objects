a6e8b8a298522e93d3fbfb333cd00adb714ca2b1
Tim <tim@timcollins.ie>
2018-12-19 20:35:09 +0000

    Added initial tests.

---
+++ src/UnitTests/DistinctTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using LinqToObjects;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class DistinctTests
+    {
+        [Test]
+        public void NullSourceNoComparer()
+        {
+            string[] source = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.Distinct());
+        }
+
+        [Test]
+        public void NullSourceWithComparer()
+        {
+            string source = null;
+            Assert.Throws<ArgumentNullException>(() => source.Distinct(StringComparer.Ordinal));
+        }
+    }
+}
